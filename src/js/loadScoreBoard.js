2ba7601dab09b4ac05025215d5dd188ef3b07525
Rasmus Falk <rf222fu@student.lnu.se>
2017-11-27 21:42:01 +0100

    Fixed some things on the scoreboard

--- src/js/loadScoreBoard.js
+++ src/js/loadScoreBoard.js
@@ -4,23 +4,24 @@ function loadScoreBoard () {
   let template = document.importNode(templateScoreBoard.content, true)
   document.body.appendChild(template)
 
-  let li = document.createElement('li')
-  let scoreList = document.querySelector('ul')
-
   let player = window.localStorage.getItem('player')
   let playerObj = JSON.parse(player)
 
+  let li = document.querySelectorAll('li')[0]
   li.textContent = playerObj.name
 
   if (playerObj.time > 60) {
     let min = Math.floor(playerObj.time / 60)
     let sec = playerObj.time % 60
-    li.textContent += ` - ${min} minutes & ${sec} seconds`
+
+    if (min === 1) {
+      li.textContent += `- ${min} minute, ${sec} seconds`
+    } else {
+      li.textContent += ` - ${min} minutes, ${sec} seconds`
+    }
   } else {
     li.textContent += ` - ${playerObj.time} seconds`
   }
-
-  scoreList.appendChild(li)
 }
 
 // Exports
